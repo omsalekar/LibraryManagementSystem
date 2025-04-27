d2583411e9d906a350fc0b3948f751445ea2de67
omsalekar <omsalekar6052@gmil.com>
2025-04-27 18:36:57 +0530

    Added Library Management Project

---
+++ LibraryManagement/LibraryManagement/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LibraryManagement;
+
+Console.WriteLine("Hello, World!");
+
+
+Book book = new Book();
+book.Addbook();
+book.DisplayAllBookDetails();
+//book.Edit_book();
+//book.Delete_Book();
+
+Member member = new Member();
+member.AddMember();
+member.DisplaymemberDetails();
+//member.Delete_Member();
+
+
+Transactions transactions = new Transactions();
+transactions.Borrow_Book();
---
+++ LibraryManagement/LibraryManagement/Transactions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement
+{
+    public class Transactions
+    {
+       public int BOOK_ID ;
+        public int MEMBER_ID;
+        public DateTime Borrow_Date;
+        public DateTime Return_Date;
+
+        public void Borrow_Book()
+        {
+            Transactions transactions = new Transactions();
+
+            Dbsource dbsource = new Dbsource();
+
+            Console.WriteLine("Enter  Book ID : ");
+            transactions.BOOK_ID = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Member ID : ");
+            transactions.MEMBER_ID = Convert.ToInt32(Console.ReadLine());
+
+            transactions.Borrow_Date = DateTime.Now;
+            transactions.Return_Date = DateTime.Now.AddDays(10);
+
+            dbsource.BOOROW_TRANSACTION(transactions);
+        }
+    }
+}
